fe8ed510ba9017e4805ba041854225384ab67fa6
nathan <nathankfoss@gmail.com>
2020-05-14 20:26:58 -0500

    Connect service and component functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,47 +16,25 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient, private postsService: PostsService) { }
 
-  ngOnInit() { this.fetchPosts() }
+  ngOnInit() {
+    this.onFetchPosts();
+  }
 
   onCreatePost(postData: Post) {
     this.postsService.createAndStorePost(postData.title, postData.content)
   }
 
   onFetchPosts() {
-    this.fetchPosts;
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    })
   }
-
 
   onClearPosts() {
     // Send Http request
   }
 
-  // Before we subscribe to our response, we transform the response data in a certain way. We want to put all the posts into an array...
-  // We pipe/map the data... fur each key (which corresponds to a post). 
-  // The spread operator '...' will pull out all key/value pairs of that object (in our case, the title and content KV pairs)
-  // We also append an ID field, which holds the unique identifier provided by FireBase. This handy if we want to delete a single post.
-  private fetchPosts() {
-    this.isFetching = true;
 
-    this.http
-      .get<{ [key:string]: Post }> ( // Here, we specify the type of object contained in the response body. A key (that can be INTERPRETTED as a string) with an object that we can use the Post interface with.
-        'https://angular-firebase-practic-4e35f.firebaseio.com/posts.json' // only one arg for GET requests
-        )
-      .pipe( map( responseData => {    // So we expect the data to be a key (that can be interpretted as string)
-        const postsArray: Post[] = [];                                    // and the value is an object we can use the 'Post' interfac for
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postsArray.push({ ...responseData[key], id: key })
-          }
-        }
-        return postsArray;
-      }
-      ))
-
-      .subscribe(posts => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-      })
-  }
-  
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,34 +1,56 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
+import { map } from 'rxjs/operators';
 
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostsService {
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     createAndStorePost(title: string, content: string) {
-    // Send Http request
-    const postData: Post = {title: title, content: content}
-    this.http
-    .post<{ name: string }>(  // takes two arguments...
+        // Send Http request
+        const postData: Post = { title: title, content: content }
+        this.http
+            .post<{ name: string }>(  // takes two arguments...
 
-      'https://angular-firebase-practic-4e35f.firebaseio.com/posts.json',
-      // 1. The url you find for your realtime database + /posts.json. This second part is necessary to send post requests.
-      // This data will end up in a folder called 'posts'.
+                'https://angular-firebase-practic-4e35f.firebaseio.com/posts.json',
+                // 1. The url you find for your realtime database + /posts.json. This second part is necessary to send post requests.
+                // This data will end up in a folder called 'posts'.
 
-      postData 
-      // 2. The actual data... ie the request body. 
-      // The Angular HTTP client will turn this from a JS object into a JSON object.
-    )
-    .subscribe(responseData => { // You must subscribe to the observable that wraps the request, otherwise Angular will never send the request.
-      console.log(responseData);
-    });
+                postData
+                // 2. The actual data... ie the request body. 
+                // The Angular HTTP client will turn this from a JS object into a JSON object.
+            )
+            .subscribe(responseData => { // You must subscribe to the observable that wraps the request, otherwise Angular will never send the request.
+                console.log(responseData);
+            });
 
-    } 
+    }
 
+    // Before we pass our response to the component, we transform the response data in a certain way. We want to put all the posts into an array...
+    // We pipe/map the data... fur each key (which corresponds to a post). 
+    // The spread operator '...' will pull out all key/value pairs of that object (in our case, the title and content KV pairs)
+    // We also append an ID field, which holds the unique identifier provided by FireBase. This handy if we want to delete a single post.
     fetchPosts() {
+        return this.http
+            .get<{ [key: string]: Post }>( // Here, we specify the type of object contained in the response body. A key (that can be INTERPRETTED as a string) with an object that we can use the Post interface with.
+                'https://angular-firebase-practic-4e35f.firebaseio.com/posts.json' // only one arg for GET requests
+            )
+            .pipe(map(responseData => {    // So we expect the data to be a key (that can be interpretted as string)
+                const postsArray: Post[] = [];                                    // and the value is an object we can use the 'Post' interfac for
+                for (const key in responseData) {
+                    if (responseData.hasOwnProperty(key)) {
+                        postsArray.push({ ...responseData[key], id: key })
+                    }
+                }
+                return postsArray;
+            }
+            )
+    )
+
+            
 
     }
 }
